36f677ca5e687ce7fead977a6b848c8a7eebc634
buithu <sadar19919@gmail.com>
2017-10-23 09:01:31 +0700

    Update dropdown.js

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,21 +1,4 @@
- $(".drop .navbar ul li span").click(function() 
-								                {
-								                    var span = $(this);
-								                    var li = $(this).parent();
-								                    if (li.hasClass("selected")) {
-												    $('ul li').removeClass('selected');
-								                 
-								                        return false;
-								                    } 
-								                    else {
-								                       
-								                        
-								                        $('ul li').removeClass('selected');
-								                       
-								                        li.addClass("selected");
-								                    }
 
-								                });
  $(".menuleft .navbar ul li span").click(function(){
 
  	var span = $(this);
