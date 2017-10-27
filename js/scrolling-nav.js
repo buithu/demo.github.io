d7593d7951c1e63dabb8578dd38892862327e1d2
buithu <sadar19919@gmail.com>
2017-10-27 10:50:41 +0700

    Update scrolling-nav.js

--- js/scrolling-nav.js
+++ js/scrolling-nav.js
@@ -1,14 +1,6 @@
 
 
-window.onscroll = function() {scrollFunction1()};
 
-function scrollFunction1() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        document.getElementsByClassName("menu")[0].classList.add("navbar-fixed-top color");
-    } else {
-        document.getElementsByClassName("menu")[0].classList.remove("navbar-fixed-top color");
-    }
-}
 
 
 window.onscroll = function() {scrollFunction()};
@@ -16,8 +8,10 @@ window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
         document.getElementById("backtop").style.display = "block";
+        document.getElementsByClassName("menu")[0].classList.add("navbar-fixed-top color");
     } else {
         document.getElementById("backtop").style.display = "none";
+        document.getElementsByClassName("menu")[0].classList.add("navbar-fixed-top color");
     }
 }
 
