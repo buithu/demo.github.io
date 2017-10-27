e5d0b54a336a63f02a9aa05940f6928ad8a92287
buithu <sadar19919@gmail.com>
2017-10-27 10:24:31 +0700

    Update scrolling-nav.js

--- js/scrolling-nav.js
+++ js/scrolling-nav.js
@@ -6,3 +6,20 @@ $(window).bind('scroll', function () {
     }
 });
 
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("backtop").style.display = "block";
+    } else {
+        document.getElementById("backtop").style.display = "none";
+    }
+}
+
+
+function back() {
+
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
