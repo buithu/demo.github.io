d40e2710bf0a4196cc6bf477ff9002125bc1a675
buithu <sadar19919@gmail.com>
2017-10-31 10:12:15 +0700

    Update dropdown.js

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,22 +1,38 @@
 
- $(".drop .navbar ul li span").click(function() 
-								                {
-								                    var span = $(this);
-								                    var li = $(this).parent();
-								                    if (li.hasClass("selected")) {
-												    $('ul li').removeClass('selected');
-								                 
-								                        return false;
-								                    } 
-								                    else {
-								                       
-								                        
-								                        $('ul li').removeClass('selected');
-								                       
-								                        li.addClass("selected");
-								                    }
+var menu = document.querySelectorAll('div.col-lg-7 > ul > li > span');
+ 
 
-								                }); 
+            for (var i = 0; i < menu.length; i++)
+            {
+
+                menu[i].addEventListener("click", function()
+                {
+                	
+
+                    var menuList = document.querySelectorAll('div.col-lg-7 > ul > li > ul');
+                    for (var j = 0; j < menuList.length; j++) {
+                        
+                        if ( menuList[j].style.display == "block") {
+                        	menuList[j].style.display = "none";
+                        }
+                        else{
+                        	
+                        	this.nextElementSibling.style.display = "block";
+                        }
+                    }
+ 					
+                   
+                    
+                });
+            }
+            window.onclick = function(event) {
+		    if (!event.target.matches('.spanmn')) {
+			    	for (var d = 0; d < menu.length; d++){
+			      document.getElementsByClassName("ka1")[d].style.display = "none";
+			    	}
+		    	}
+		    }
+	    
 $(".menuleft .navbar ul li span").click(function(){
 
  	var span = $(this);
