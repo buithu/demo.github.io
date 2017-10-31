0f338c56cff3593cdc5849eef021be1929ca2154
buithu <sadar19919@gmail.com>
2017-10-31 11:01:40 +0700

    Update dropdown.js

--- js/dropdown.js
+++ js/dropdown.js
@@ -33,17 +33,5 @@ var menu = document.querySelectorAll('div.col-lg-12 > ul > li > span');
 		    	}
 		    }
 	    
-$(".menuleft .navbar ul li span").click(function(){
 
- 	var span = $(this);
-	var li = $(this).parent();
-	if (li.hasClass("selected")) {
-		span.addClass("glyphicon-minus");
-								                 
-	return false;
-	} 
-	 else {
-    span.removeClass("glyphicon-minus");
-	 }
- });
  
