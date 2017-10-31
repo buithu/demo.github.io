17fb11a05d8401f5fa86eeac998de4c35c0a2c7b
buithu <sadar19919@gmail.com>
2017-10-31 11:03:04 +0700

    Update number.js

--- js/number.js
+++ js/number.js
@@ -30,3 +30,17 @@
       });
 
     });
+
+$(".menuleft .navbar ul li span").click(function(){
+
+ 	var span = $(this);
+	var li = $(this).parent();
+	if (li.hasClass("selected")) {
+		span.addClass("glyphicon-minus");
+								                 
+	return false;
+	} 
+	 else {
+    span.removeClass("glyphicon-minus");
+	 }
+ });
